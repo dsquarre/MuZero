5f9274eb72d1e6f71b5157c73552df73a3014c6d
dsquarre <dsquar0e@gmail.com>
2025-09-24 16:33:01 +0530

    made resnets and refactoring

--- muzero.py
+++ muzero.py
@@ -1,33 +1,9 @@
 import torch
-from env.env import Env
+from resnet import ResNet18
 import torch.nn as nn
 from collections import deque
 import math
-class Network(nn.Module):
-    def __init__(self):
-        super(Network, self).__init__()
-        
-        self.representation = nn.Sequential(
-            nn.Conv2d(),
-            nn.ReLU(),
-            nn.Conv2d()
-        )
-        self.dynamics = nn.Sequential(
-            nn.Conv2d(),
-            nn.ReLU(),
-            nn.Conv2d()
-        )
-        self.prediction = nn.Sequential(
-            nn.Conv2d(),
-            nn.ReLU(),
-            nn.Conv2d()
-        )
-    def f(self,x):
-        return self.prediction(x)
-    def g(self,x):
-        return self.dynamics(x)
-    def h(self,x):
-        return self.representation(x)
+
 # okay so mcts starts from a state which basically creates a parent node
 # it will check for children, if not expand
 # expand will add links to Node.children and each of these links will have a start-parent node, end- child node, action and properties
@@ -57,25 +33,18 @@ class Node:
         self.total_visits = 0
         self.Policy = None
         self.value = float(0)
-    #create 2 channels for each player instead of turn.    
-    def action_space(self,dots,turn):
-        mu = MuZero(dots)
-        actions = []        
-        for i in range(2*dots*(dots-1)):
-            actions.append(mu.Action(dots,i,turn))
-        actions.append(mu.Action(dots,-1,turn))
-        return actions
+   
     #only use f and create all children. when to use g? when selecting best child and action has been chosen. 
-    def add_links(self,network,dots,turn):
+    def add_links(self,network,Env):
         pi,v = network.f(self.state)
         policy = torch.softmax(pi,dim=1).squeeze(0)
         self.v = v.item()
         self.policy = policy.item()
-        for a in self.action_space(dots,turn):
+        for a in Env.action_space():
             child = Link(self,a)
             self.children.append(child)
 
-    def expand(self,link,network,dots,turn):
+    def expand(self,link,network,Env):
         action = link.action
         r,next_state = network.g(torch.stack((self.state,action),dim=0))
         link.R = r
@@ -85,7 +54,7 @@ class Node:
         policy = torch.softmax(pi,dim=1).squeeze(0)
         next_node.v = v.item()
         next_node.policy = policy.item()
-        for a in self.action_space(dots,turn):
+        for a in Env.action_space():
             child = Link(next_node,a)
             next_node.children.append(child)
         return next_node
@@ -117,7 +86,7 @@ class Node:
 
 class MCTS:
     #fix this-> basically return all those shits masking the values
-    def think(self,state,dots,turn,network,valid_actions,sims=800):
+    def simulate(self,state,dots,turn,network,valid_actions,sims=800):
         root =Node(state)
         root.add_links(network,dots,turn)
         link = None
@@ -142,9 +111,9 @@ class MCTS:
         #get valid actions, mask all invalid actions to 0, reconvert to probability.
         #use ucb/puct formula to get max from valid actions and return everything
         #how am i supposed to mask bad actions???
-        return self.getresult(valid_actions,root)
+        return self.extract(valid_actions,root)
     
-    def getresult(self,valid_actions,root,t=1):
+    def extract(self,valid_actions,root,t=1):
         predpi = root.policy
         Pi =[]
         predv = root.V
@@ -183,127 +152,36 @@ class MCTS:
         return G   
         
 class MuZero:
-    def __init__(self,dots):
+    def __init__(self,Env):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.network = Network().to(self.device)
+        self.network = ResNet18(Env.getparams())
+        self.network.to(self.device)
+        self.Env = Env
         self.replay_buffer = deque(maxlen=10000)
-        self.dots = dots
-    
-    #channel 1: player1moves, channel2: player2moves, channel3:player1boxes,channel4:player2boxes,channel5: turn or color.
-    # it has to be one hot encoded, numbers alone cannot help nn differentiate well enough.
-    def observation(self,env,turn):
-        grid = torch.zeros([2*env.dots-1,2*env.dots-1],dtype=torch.int8,device=self.device,requires_grad=False)
-        if turn ==1:
-            last = torch.ones([2*env.dots-1,2*env.dots-1],dtype=torch.int8,device=self.device,requires_grad=False)
-        else:
-            last = grid
-        grid = torch.stack((grid,grid,grid,grid,last),dim=0) #5 grids stacked
-        grid_index,boxindex=0,0
-        for i in range(0,2*env.dots-1,1):
-            if i%2 ==0:
-                for j in range(1,2*env.dots-1,2):
-                    if env.grid[grid_index] == 1:
-                        grid[0,i,j] = 1
-                    elif env.grid[grid_index] == -1:
-                        grid[1,i,j] = 1
-                    grid_index+=1
-            else:
-                for j in range(0,2*env.dots-1,1): 
-                    if j%2 == 0:
-                        if env.grid[grid_index] == 1:
-                            grid[0,i,j] = 1
-                        elif env.grid[grid_index] == -1:
-                            grid[1,i,j] = 1
-                        grid_index+=1
-                    else:
-                        if env.boxes[boxindex] == 1:
-                            grid[2,i,j] = 1
-                        elif env.boxes[boxindex] == -1:
-                            grid[3,i,j] = 1
-                        boxindex+=1
-        return grid
-    #action is also one hot encoded with first channel as player one move and second channel as player 2 move.
-    #there's a possibility of pass move(all values 0) if action = -1
-    def Action(self,dots,action,turn):
-        env = Env(dots)
-        env.step(action,turn)
-        grid = torch.zeros([2,2*dots-1,2*env.dots-1],dtype=torch.int8,device=self.device,requires_grad=False)
-        if action==-1:
-            return grid
-        # I can remove this O(n) complexity to O(1) using maths. will do it later.
-        for i in range(0,2*dots-1,1):
-            if i%2 ==0:
-                for j in range(1,2*dots-1,2):
-                    if env.grid[grid_index] == 1:
-                        grid[0,i,j] = 1
-                    elif env.grid[grid_index] == -1:
-                        grid[1,i,j] = 1
-                    grid_index+=1
-            else:
-                for j in range(0,2*dots-1,1): 
-                    if j%2 == 0:
-                        if env.grid[grid_index] == 1:
-                            grid[0,i,j] = 1
-                        elif env.grid[grid_index] == -1:
-                            grid[1,i,j] = 1
-                        grid_index+=1
-                """ else:
-                        if env.boxes[boxindex] == 1:
-                            grid[2,i,j] = 1
-                        elif env.boxes[boxindex] == -1:
-                            grid[3,i,j] = 1
-                        boxindex+=1"""
-        return grid
-    
-    def State(self,O):
-        history = []
-        if len(O)<=8:
-            history = [O[0]]*(8-len(O))
-            for j in range(len(O)):
-                history.append(O[j])
-                
-        else:
-            history = []
-            for j in range(len(O)-8,len(O)):
-                history.append(O[j])
-        return self.network.h(torch.stack(history,dim=0))
-    
+
     def play(self,games=800):
-        env = Env(self.dots)
         for g in range(games):
-            O = deque(maxlen=8)
+            O = []
             A = []
             R = []
             predPi = []
             calcPi = []
             predV = []
             predR = []
-            env.reset()
-            turn = 1
-            O.append(self.observation(env))
-            state = self.State(O)
-            S = []
+            self.Env.reset()
+            obs = self.env.Observation()
             mcts = MCTS(self.dots)
-            valid_actions = env.action_space()
-            while not env.gameover():
-                S.append(state)
-                predpi,calcpi,predv,predr,action = mcts.think(state,env.dots,turn,self.network,valid_actions,sims=800)
+            while not self.Env.gameover():
+                O.append(obs)
+                predpi,calcpi,predv,predr,action = mcts.simulate(obs,self.Env,self.network,sims=800)
                 predPi.append(predpi)
                 calcPi.append(calcpi)
                 predV.append(predv)
                 predR.append(predr)
                 A.append(action)
-                if action>0:
-                    reward = env.step(action,turn)
-                if turn==reward and action>0:
-                    valid_actions = None
-                else:
-                    valid_actions = env.action_space()
-                turn*=1
-                reward = 0 if not env.gameover() else env.result() # for dotgame specific
-                O.append(self.observation(env))
+                reward = self.Env.step(action)
+                obs = self.Env.Observation()
                 R.append(reward)
-                state = self.State(O)
             #have to store a done variable too to show that one game is finished and next is starting.
             for i in range(len(A)):
                 self.replay_buffer.append({
@@ -366,6 +244,3 @@ class MuZero:
             
             
         
-bot = MuZero()
-env = Env(3)
-bot.play(env,1)
--- resnet.py
+++ resnet.py
@@ -1,19 +1,19 @@
 import torch.nn as nn
 
 class BasicBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1):
+    def __init__(self, obs_channels, state_channels, stride=1):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv1 = nn.Conv2d(obs_channels, state_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(state_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(state_channels, state_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(state_channels)
         
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
+        if stride != 1 or obs_channels != state_channels:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
+                nn.Conv2d(obs_channels, state_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(state_channels)
             )
 
     def forward(self, x):
@@ -25,6 +25,73 @@ class BasicBlock(nn.Module):
         out += self.shortcut(x)
         out = self.relu(out)
         return out
+
+class ResNet18(nn.Module):
+    def __init__(self, obs_channels,gridH,gridW,actionspace,state_channels,state_action_channels):
+        super(ResNet18, self).__init__()
+        self.obs_channels = obs_channels
+        self.rep_conv = nn.Conv2d(obs_channels, 256, kernel_size=3, stride=1, padding=1, bias=False)
+        self.pred_conv = nn.Conv2d(state_channels, 256, kernel_size=3, stride=1, padding=1, bias=False)
+        self.dyn_conv = nn.Conv2d(state_action_channels, 256, kernel_size=3, stride=1, padding=1, bias=False)
+        self.resnet = nn.Sequential(
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            self._make_layer(BasicBlock, 256, 2, stride=1),
+            self._make_layer(BasicBlock, 256, 2, stride=2),
+            self._make_layer(BasicBlock, 256, 2, stride=2),
+            self._make_layer(BasicBlock, 256, 2, stride=2),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            )
+        self.hc = nn.Linear(256, state_channels)
+        self.fc1 = nn.Sequential(
+            nn.Linear(256 * gridH * gridW, 128),
+            nn.ReLU(),
+            nn.Linear(128, actionspace)         # Policy 
+            )
+        self.fc2 = nn.Sequential(
+            nn.Linear(256 * gridH * gridW, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)         # Value 
+            )
+        self.gc1 = nn.Linear(256, state_channels)
+        self.gc2 = nn.Sequential(
+            nn.Flatten(), #Output
+            nn.Linear(256*gridH*gridW,128),
+            nn.ReLU(),
+            nn.Linear(128,1) #reward
+            )      
+        
+    def _make_layer(self, block, state_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.obs_channels, state_channels, stride))
+            self.obs_channels = state_channels
+        return nn.Sequential(*layers)
+
+    def h(self, x):
+        #x = observation
+        #out = state
+        out = self.rep_conv(x)
+        out = self.resnet(out)
+        return self.hc(out)
+    
+    def f(self,x):
+        #x = state
+        #out = policy,value
+        out = self.pred_conv(x)
+        out = self.resnet(out)
+        out = nn.Flatten()(out)
+        return self.fc1(out),self.fc2(out)
+    
+    def g(self,x):
+        #x = state || action
+        #return nextstate,reward
+        out = self.dyn_conv(x)
+        out = self.resnet(out)
+        return self.gc1(out),self.gc2(out)
+    
 #concat or add channels for history obs?
 #create different output layers for f(state)->(policy,value),g(action,state)->(state,reward),h(obs)->(state)
 # change fc layer after avgpool ofc and avg pooling or maxpool? and what size? read paper..
@@ -34,57 +101,3 @@ class BasicBlock(nn.Module):
 #that will give me 256(state),1(reward)
 #f will recieve (state)256 as input
 #that will give me action_space(4*dots*(dots-1)+1) , value(1)
-class ResNet18(nn.Module):
-    def __init__(self, in_channels,out_channels):
-        super(ResNet18, self).__init__()
-        self.in_channels = in_channels
-        self.conv1 = nn.Conv2d(in_channels, 256, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(256)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(BasicBlock, 256, 2, stride=1)
-        self.layer2 = self._make_layer(BasicBlock, 256, 2, stride=2)
-        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2)
-        self.layer4 = self._make_layer(BasicBlock, 256, 2, stride=2)
-        
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(256, out_channels)
-        
-    def _make_layer(self, block, out_channels, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_channels, out_channels, stride))
-            self.in_channels = out_channels
-        return nn.Sequential(*layers)
-
-    def h(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.maxpool(out)
-        
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        
-        out = self.avgpool(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
-    
-    def f(self,x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.maxpool(out)
-        
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        
-        out = self.avgpool(out)
-        out = out.view(out.size(0), -1)
-        out = 
